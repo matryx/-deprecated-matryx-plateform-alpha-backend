a895e2bca52ccfc62d3a0e8005a9deaa65afa42a
Max Howard <tmhoward@ucsd.edu>
2017-11-21 02:18:03 -0800

    added config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    pg: {
+        host: 'matryx-alpha-plateform-postgres.ct6gkqbi3aat.us-west-1.rds.amazonaws.com',
+        port: 5432,
+        user: 'matryx_admin',
+        password: 'SbQnAyf3S4MmJLBv',
+        database: 'matryx_db',
+        ssl: true,
+    },
+    geth: {
+        host: 'matryx-plateform-alpha-geth',
+        port: 8545,
+    },
+    // geth: {
+    //     host: '13.57.143.35',
+    //     port: 8545,
+    // },
+    port: 8081,
+};
